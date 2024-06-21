f29fe52cbc84d07ae221b0b1ff6d364bbfaefc53
José Vitor <jose-vitor-7@hotmail.com>
2024-06-21 01:38:38 -0300

    try navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 
 <script setup lang="ts">
   const count = ref(1)
+
+  // custom object
+  const handleToDoNavigation = () => navigateTo({
+    path: '/todo',
+  })
 </script>
 
 <template>
-  <button @click="count++" class="btn">count: {{ count }}</button>
+  <div className="flex flex-col gap-2 w-max">
+    <button @click="count++" class="btn">count: {{ count }}</button>
+  
+    <NuxtLink to="/todo" class="link">ToDo Page | Link component</NuxtLink>
+    <button @click="handleToDoNavigation" class="link">ToDo Page | Programmatic Navigation</button>
+  </div>
 </template>
--- pages/todo.vue
+++ pages/todo.vue
@@ -27,19 +27,32 @@
 
     todos.value = newTodos
   }
+
+  // raw url
+  const handleCounterNavigation = () => navigateTo('/')
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 w-max">
-    <h1 class="title">Create todo</h1>
-    <button v-on:click="createTodo()" class="btn">Create</button>
-    <h1 class="title">Todos</h1>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" :style="todo.isChecked ? 'text-decoration: line-through' : ''">
-        {{ todo.title }}
-  
-        <input type="checkbox" name="" id="" v-bind:checked="todo.isChecked" @change="toggleChecked(todo.id)" />
-      </li>
-    </ul>
+  <div class="flex flex-col gap-4 w-max">
+    <div class="flex flex-col gap-2">
+      <h1 class="title">Create todo</h1>
+      <button v-on:click="createTodo()" class="btn">Create</button>
+      <hr />
+    </div>
+    <div class="flex flex-col gap-2">
+      <h1 class="title">Todos</h1>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id" :style="todo.isChecked ? 'text-decoration: line-through' : ''">
+          {{ todo.title }}
+    
+          <input type="checkbox" name="" id="" v-bind:checked="todo.isChecked" @change="toggleChecked(todo.id)" />
+        </li>
+      </ul>
+      <hr />
+    </div>
+    <div class="flex flex-col gap-2">
+      <NuxtLink to="/" class="link">Counter Page | Link component</NuxtLink>
+      <button @click="handleCounterNavigation" class="link">Counter Page | Programmatic Navigation</button>
+    </div>
   </div>
 </template>
